3cbc1a0f64a896055f663d514577d88c87c183b9
Cauã Rodrigues <caua.rodrigues08@aluno.ifce.edu.br>
2025-07-17 17:45:50 -0300

    Inicio da implementação de um schema para as fichas

---
+++ models/sheet.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+const sheetSchema = new mongoose.Schema ({
+    informacoes: {
+        nome: {type: String, required: true},
+        profissao: String,
+        naturalidade: String,
+        residencia: String,
+        pronome: String,
+        idade: Number
+    },
+    caracteristicas: {
+        status: {
+            FOR: Number,
+            CON: Number,
+            DES: Number,
+            INT: Number,
+            TAM: Number,
+            POD: Number,
+            APA: Number,
+            EDU: Number,
+            SORTE: {
+                inicial: Number,
+                atual: Number
+            }
+        },
+        hp: Number,
+        mp: Number,
+        sanidade: {
+            inicial: Number,
+            atual: Number
+        },
+        estados: {
+            insanidadeTemp: {type: Boolean, default: false},
+            insanidadeIndef: {type: Boolean, default: false},
+            lesaoGrave: { type: Boolean, default: false },
+            inconsciente: { type: Boolean, default: false },
+            morrendo: { type: Boolean, default: false }
+        }
+    },
+    habilidades: {
+        //Resolvendo template de habilidade. Conflito com o template usado em skills.js, pois este é incompleto
+    },
+    combate: {
+        arma1: {
+            nome: String,
+            valor: Number,
+            dano: String,
+            ataques: Number,
+            alcance: Number,
+            pente: Number,
+            falha: Number
+        },
+        arma2: {
+            nome: String,
+            valor: Number,
+            dano: String,
+            ataques: Number,
+            alcance: Number,
+            pente: Number,
+            falha: Number
+        },
+        arma3: {
+            nome: String,
+            valor: Number,
+            dano: String,
+            ataques: Number,
+            alcance: Number,
+            pente: Number,
+            falha: Number
+        },
+    }
+})
